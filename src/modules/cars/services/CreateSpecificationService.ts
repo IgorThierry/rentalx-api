bd0ea2f69fe0f490917e2d47995a0689d717410d
Igor Thierry <igorthierry15@gmail.com>
2022-12-23 14:22:38 -0300

    feat: criando specification

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,5 +1,24 @@
+import { AppError } from '../../../errors/AppError';
+import { ISpecificationsRepository } from '../repositories/ISpecificationsRepository';
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() {}
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+  execute({ name, description }: IRequest) {
+    const specificationAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new AppError('Specification already exists!');
+    }
+
+    this.specificationsRepository.create({ name, description });
+  }
 }
 
 export { CreateSpecificationService };
