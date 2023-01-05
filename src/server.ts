614416b575c52fda3245d038effae4c78f25233c
Igor Thierry <igorthierry15@gmail.com>
2023-01-05 11:11:01 -0300

    feat: adicionando e linkando container postgres

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,8 @@ import { appErrorHandler } from './errors/appErrorHandler';
 import { router } from './routes';
 import swaggerFile from './swagger.json';
 
+import './database';
+
 const app = express();
 
 app.use(express.json());
