750982fe73793c0b13160707328377e42c3972a0
Igor Thierry <igorthierry15@gmail.com>
2023-01-10 14:01:29 -0300

    feat: autenticação nas rotas

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,46 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+import { container } from 'tsyringe';
+
+import { authConfig } from '../config/auth';
+import { AppError } from '../errors/AppError';
+import { UsersRepository } from '../modules/accounts/repositories/implementations/UsersRepository';
+
+interface ITokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('JWT token is missing', 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = verify(token, authConfig.jwt.secret);
+
+    const { sub: user_id } = decoded as ITokenPayload;
+
+    const usersRepository = new UsersRepository();
+    const user = usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User does not exists.');
+    }
+
+    return next();
+  } catch {
+    throw new AppError('Invalid JWT token', 401);
+  }
+}
+
+export { ensureAuthenticated };
