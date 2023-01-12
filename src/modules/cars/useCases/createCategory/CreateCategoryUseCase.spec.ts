c4c108af6034921c226ffe06a51eb074373ca6e8
Igor Thierry <igorthierry15@gmail.com>
2023-01-12 11:45:41 -0300

    feat: Teste de criação de categoria

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,46 @@
+import { AppError } from '../../../../errors/AppError';
+import { InMemoryCategoriesRepository } from '../../repositories/in-memory/InMemoryCategoriesRepository';
+import { CreateCategoryUseCase } from './CreateCategoryUseCase';
+
+let createCategoryUseCase: CreateCategoryUseCase;
+let inMemoryCategoriesRepository: InMemoryCategoriesRepository;
+
+describe('Create Category', () => {
+  beforeAll(() => {
+    inMemoryCategoriesRepository = new InMemoryCategoriesRepository();
+    createCategoryUseCase = new CreateCategoryUseCase(
+      inMemoryCategoriesRepository,
+    );
+  });
+
+  it('Should be able to create a new category', async () => {
+    const category = {
+      name: 'Category Test',
+      description: 'Category description Test',
+    };
+
+    await createCategoryUseCase.execute(category);
+
+    const categoryCreated = await inMemoryCategoriesRepository.findByName(
+      category.name,
+    );
+
+    expect(categoryCreated).toHaveProperty('id');
+    expect(categoryCreated).toHaveProperty('created_at');
+    expect(categoryCreated.name).toBe(category.name);
+    expect(categoryCreated.description).toBe(category.description);
+  });
+
+  it('Should not be able to create a new category with an existing name', async () => {
+    await expect(async () => {
+      const category = {
+        name: 'Category Test',
+        description: 'Category description Test',
+      };
+
+      await createCategoryUseCase.execute(category);
+
+      await createCategoryUseCase.execute(category);
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
