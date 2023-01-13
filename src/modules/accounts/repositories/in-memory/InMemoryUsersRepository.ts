15bd1b7910c2e2edf4d383f83c84992ba93a17b9
Igor Thierry <igorthierry15@gmail.com>
2023-01-13 10:31:51 -0300

    feat: teste de autenticação

---
+++ src/modules/accounts/repositories/in-memory/InMemoryUsersRepository.ts
@@ -0,0 +1,37 @@
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+class InMemoryUsersRepository implements IUsersRepository {
+  private users: User[] = [];
+
+  async create(data: ICreateUserDTO): Promise<void> {
+    const { name, email, password, driver_license, avatar, id } = data;
+
+    const user = new User();
+
+    Object.assign(user, { name, email, password, driver_license, avatar });
+
+    if (!id) {
+      user.created_at = new Date();
+      this.users.push(user);
+      return;
+    }
+
+    const userIndex = this.users.findIndex((user) => user.id === id);
+
+    if (userIndex === -1) return;
+
+    this.users[userIndex] = { ...this.users[userIndex], ...user };
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.users.find((user) => user.email === email);
+  }
+
+  async findById(id: string): Promise<User> {
+    return this.users.find((user) => user.id === id);
+  }
+}
+
+export { InMemoryUsersRepository };
---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,84 @@
+import { AppError } from '../../../../errors/AppError';
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { InMemoryUsersRepository } from '../../repositories/in-memory/InMemoryUsersRepository';
+import { CreateUserUseCase } from '../createUser/CreateUserUseCase';
+import { AuthenticateUserUseCase } from './AuthenticateUserUseCase';
+
+let authenticateUserUseCase: AuthenticateUserUseCase;
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let createUserUseCase: CreateUserUseCase;
+
+describe('Authenticate User', () => {
+  beforeAll(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    authenticateUserUseCase = new AuthenticateUserUseCase(
+      inMemoryUsersRepository,
+    );
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+  });
+
+  it('Should be able to authenticate a user', async () => {
+    const userData: ICreateUserDTO = {
+      name: 'user',
+      email: 'user@test.com',
+      password: '1234',
+      driver_license: '1234',
+    };
+
+    await createUserUseCase.execute(userData);
+
+    const result = await authenticateUserUseCase.execute({
+      email: userData.email,
+      password: userData.password,
+    });
+
+    expect(result).toHaveProperty('token');
+    expect(result).toHaveProperty('user');
+    expect(result.user).toHaveProperty('name');
+    expect(result.user).toHaveProperty('email');
+  });
+
+  it('Should not be able to authenticate an nonexistent user', async () => {
+    expect(async () => {
+      await authenticateUserUseCase.execute({
+        email: 'false@email.com',
+        password: '1234',
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('Should not be able to authenticate with incorrect password', async () => {
+    expect(async () => {
+      const userData: ICreateUserDTO = {
+        name: 'user',
+        email: 'user@test.com',
+        password: '1234',
+        driver_license: '1234',
+      };
+
+      await createUserUseCase.execute(userData);
+
+      await authenticateUserUseCase.execute({
+        email: userData.email,
+        password: 'incorrect-password',
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+  it('Should not be able to authenticate with incorrect email', async () => {
+    expect(async () => {
+      const userData: ICreateUserDTO = {
+        name: 'user',
+        email: 'user@test.com',
+        password: '1234',
+        driver_license: '1234',
+      };
+
+      await createUserUseCase.execute(userData);
+
+      await authenticateUserUseCase.execute({
+        email: 'incorrect-email',
+        password: userData.password,
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
