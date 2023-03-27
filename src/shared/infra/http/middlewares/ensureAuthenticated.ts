9005bd983636a10b86f27da1ba074265b272d091
Igor Thierry <igorthierry15@gmail.com>
2023-03-27 09:02:40 -0300

    feat: refresh token

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -2,7 +2,6 @@ import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 
 import { authConfig } from '@config/auth';
-import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 import { AppError } from '@shared/errors/AppError';
 
 interface ITokenPayload {
@@ -25,16 +24,9 @@ async function ensureAuthenticated(
   const [, token] = authHeader.split(' ');
 
   try {
-    const decoded = verify(token, authConfig.jwt.secret);
+    const decoded = verify(token, authConfig.jwt.secret_token);
 
     const { sub: user_id } = decoded as ITokenPayload;
-
-    const usersRepository = new UsersRepository();
-    const user = await usersRepository.findById(user_id);
-
-    if (!user) {
-      throw new AppError('User does not exists.', 401);
-    }
 
     request.user = { id: user_id };
 
