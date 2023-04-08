6e6bd60a9dfdd8b5f2e2d43d0a501e3766123e5a
Igor Thierry <igorthierry15@gmail.com>
2023-04-08 10:20:20 -0300

    fix: createConnection and admin seed

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,6 +1,6 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (): Promise<Connection> => {
+export default async (host = 'database'): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   const database =
@@ -8,7 +8,7 @@ export default async (): Promise<Connection> => {
 
   const newOptions = Object.assign(defaultOptions, {
     database,
-    host: 'database',
+    host,
   });
 
   return createConnection(newOptions);
