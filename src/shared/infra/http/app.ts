2fbfda0f4c3a2a6921c575e13861f5201683a4cd
Igor Thierry <igorthierry15@gmail.com>
2023-04-10 10:38:14 -0300

    feat: sentry, rateLimiter, static route

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,23 +1,46 @@
 import 'reflect-metadata';
 import 'dotenv/config';
+import cors from 'cors';
 import express from 'express';
 import 'express-async-errors';
-import swaggerUI from 'swagger-ui-express';
+import swaggerUi from 'swagger-ui-express';
 
+import { uploadConfig } from '@config/upload';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 import '@shared/container';
-
-import { appErrorHandler } from '@shared/infra/http/middlewares/appErrorHandler';
-import { router } from '@shared/infra/http/routes';
+import rateLimiter from '@shared/infra/http/middlewares/rateLimiter';
 
 import swaggerFile from '../../../swagger.json';
+import { appErrorHandler } from './middlewares/appErrorHandler';
+import { router } from './routes';
 
 const app = express();
+app.use(rateLimiter);
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(express.json());
 
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
+app.use('/avatar', express.static(`${uploadConfig.tmpFolder}/avatar`));
+app.use('/cars', express.static(`${uploadConfig.tmpFolder}/cars`));
+
+app.use(cors());
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(appErrorHandler);
 
